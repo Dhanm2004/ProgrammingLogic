98d0af4c5967c13ac0267209b88638d41aae88aa
Dhanm2004 <128613216+Dhanm2004@users.noreply.github.com>
2023-10-28 21:30:33 +0530

    Create revers.py

---
+++ python/revers.py
@@ -0,0 +1,9 @@
+# a list of numbers
+list1 = [1, 2, 3, 4, 1, 2, 6] 
+list1.reverse() 
+print(list1) 
+ 
+# a list of characters
+list2 = ['a', 'b', 'c', 'd', 'a', 'a'] 
+list2.reverse() 
+print(list2)
